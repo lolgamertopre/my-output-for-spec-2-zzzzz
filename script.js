63e0c35b5eeb8901d19f82558d539541df1e62b6
spec2b24 <spec2b24@snjose.local>
2026-03-18 11:41:01 +0800

    new files

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitlogin (event){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById ("password").value;
+
+    alert ("Email: "+ email);
+    alert (
+}
